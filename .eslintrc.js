d5cd4fa591c4e175d5eba52df4ac46d84c97099b
francoismassart <francois.massart@gmail.com>
2023-01-23 07:45:36 +0100

    latest version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,5 @@
 module.exports = {
   "root": true,
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "plugins": [
-    "tailwindcss",
-  ],
   "extends": [
     "plugin:tailwindcss/recommended"
   ]
---
+++ src/demo.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container sm:w-3 sm:w-4 lg:w-6">
+    <h1>Vue SFC</h1>
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
